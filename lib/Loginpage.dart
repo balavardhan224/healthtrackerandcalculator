85845e02fce4fcd5014e0339461f38bd0beb0bc6
Saikrishna_alishala <saikrishna.alishala@gmail.com>
2021-11-03 21:39:03 +0530

    initial commit

---
+++ lib/Loginpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  @override
+  _LoginpageState createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 18,
+          vertical: 4,
+        ),
+        child: Drawer(),
+      ),
+    );
+  }
+}
