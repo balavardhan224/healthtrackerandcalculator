d86479b8773e7d42fa6ff1501e8819142059f3a1
Saikrishna_alishala <saikrishna.alishala@gmail.com>
2021-11-03 23:16:17 +0530

    initial commit

---
+++ lib/loginwith.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:healthtrackerandcalculator/Loginpage.dart';
+
+class Loginwith extends StatefulWidget {
+  @override
+  _LoginwithState createState() => _LoginwithState();
+}
+
+class _LoginwithState extends State<Loginwith> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 100,
+        ),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Continue Login',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      15,
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Loginpage(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Login with Email',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
