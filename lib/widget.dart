fbbca1973b697fa4a5a8789f429bb7393ebd52e0
Saikrishna_alishala <saikrishna.alishala@gmail.com>
2021-11-22 23:41:17 +0530

    intial commit

--- lib/widget.dart
+++ lib/widget.dart
@@ -65,6 +65,13 @@ Widget buildcontainer(BuildContext context) {
               25,
             ),
           ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3)),
+          ],
           color: Colors.amber,
         ),
         child: InkWell(
